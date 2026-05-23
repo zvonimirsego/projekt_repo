28e91ba6ba8c5bdb3f7c24542e18401f37be1f86
mpontoni1 <ponmarin555@gmail.com>
2026-05-23 19:42:22 +0200

    Added new Loan route with corresponding GET method

--- server/main.py
+++ server/main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
-from server.routes import users, equipment, admin
+from server.routes import users, equipment, admin, loan
 
 app = FastAPI(title="Equipment Loan API")
 
 app.include_router(users.router)
 app.include_router(equipment.router)
 app.include_router(admin.router)
+app.include_router(loan.router)
