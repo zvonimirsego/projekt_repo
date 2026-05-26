47755f7df8c6904bbbeea4a20a8ff02f624455ff
mpontoni1 <ponmarin555@gmail.com>
2026-05-26 14:43:21 +0200

    Linting fix

--- server/routes/users.py
+++ server/routes/users.py
@@ -106,10 +106,12 @@ def return_loan(email: str, id_loan: int):
     user = Users.fetch(email)
     if not user:
         raise HTTPException(status_code=404, detail="Korisnik ne postoji")
-    
+
     success = user.returnReservation(id_loan)
     if not success:
-        raise HTTPException(status_code=400, detail="Posudba ne postoji ili je već vraćena")
+        raise HTTPException(
+            status_code=400, detail="Posudba ne postoji ili je već vraćena"
+        )
     return {"message": "Posudba uspješno vraćena"}
 
 
